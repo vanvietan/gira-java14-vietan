665c4f960562b0f99d5e965df5aee7f009af05ad
vanvietan <vanvietan2911@gmail.com>
2021-12-15 06:16:10 -0800

    1st-commit

--- src/main/java/cybersoft/javabackend/girajava14vietan/role/model/GroupRole.java
+++ src/main/java/cybersoft/javabackend/girajava14vietan/role/model/GroupRole.java
@@ -3,8 +3,11 @@ package cybersoft.javabackend.girajava14vietan.role.model;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
@@ -14,13 +17,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "gira_role")
-public class Role extends BaseEntity{
+@Table(name = "gira_group_role")
+public class GroupRole extends BaseEntity{
+	
 	
 	@Column(name = "name", unique = true)
 	private String name;
@@ -31,6 +36,10 @@ public class Role extends BaseEntity{
 	@Column(name = "code" , unique = true)
 	private String code;
 	
-	@ManyToMany(mappedBy = "roles")
-	private Set<GroupRole> groups = new LinkedHashSet<>();
+	@ManyToMany(cascade = {CascadeType.PERSIST , CascadeType.MERGE})
+	@JoinTable(name = "gira_role_group_role",
+			joinColumns = @JoinColumn(name = "group_role_id"),
+			inverseJoinColumns = @JoinColumn(name = "role_id")
+	)
+	private Set<Role> roles = new LinkedHashSet<Role>();
 }
